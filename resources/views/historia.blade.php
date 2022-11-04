f72e31d207859c78c82740aae1a16c41baea8cfe
HitYes <1321649@senati.pe>
2022-11-04 16:19:59 -0500

    Seguimos 8

--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Hola Empezamos el Proyecto</h1>
+    <!--<h1>Hola Empezamos el Proyecto</h1>--->
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -45,6 +46,9 @@
       </form>
     </div>
   </div>
+</nav>
+<nav class="navbar navbar-dark bg-primary">
+  <!-- Navbar content -->
 </nav>
 </body>
 </html>
