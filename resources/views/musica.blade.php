742f530936e4f449c49bab3ae94fa71186de54a1
HitYes <1321649@senati.pe>
2022-11-04 18:54:37 -0500

    Seguimos 13

--- resources/views/musica.blade.php
+++ resources/views/musica.blade.php
@@ -15,42 +15,31 @@
     <h2>El origen de los instrumentos musicales más famosos</h2>
     <h3>La historia de los instrumentos musicales que más amamos.</h3>
     <center><img src="https://us.123rf.com/450wm/olegback/olegback2102/olegback210200002/164671088-teclado-de-piano-aislado-sobre-fondo-blanco-elemento-de-dise%C3%B1o-de-m%C3%BAsica-vectorial.jpg?ver=6" class="img-thumbnail" alt="..."></center>
-    <p>Tenemos los mejores instrumentos de la ciudad y contamos con la garantia para tu instrumento por muchos años. </p>
-    <p>Todo comienza en algún punto de la prehistoria cuando los primeros humanos e incluso los neandertales, descubren las propiedades sonoras de ciertos utensilios de su cotidianeidad: huesos, rocas, pedazos de madera o cuerdas tensadas.</p>
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+    
+    <p><br>Todo comienza en algún punto de la prehistoria cuando los primeros humanos e incluso los neandertales, descubren las propiedades sonoras de ciertos utensilios de su cotidianeidad: <strong> huesos, rocas, pedazos de madera o cuerdas tensadas.</strong></br></p>
+<hr>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://img.freepik.com/fotos-premium/instrumentos-musicales-viento-dorado-sobre-fondo-negro-ilustracion-3d_291814-674.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
       <img src="https://c8.alamy.com/zoomses/9/efcb6c2be63749beb448434a8b067bbc/bk4fhg.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
     </div>
     <div class="carousel-item">
-      <img src="https://i.pinimg.com/736x/5c/36/e5/5c36e56795400602b641b0a74326e061.jpg" class="d-block w-100"  alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+      <img src="https://img.freepik.com/fotos-premium/instrumentos-musicales-viento-dorado-sobre-fondo-negro-ilustracion-3d_291814-674.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.pinimg.com/736x/5c/36/e5/5c36e56795400602b641b0a74326e061.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
